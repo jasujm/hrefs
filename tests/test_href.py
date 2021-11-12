e9de9abca085ac654e072695c66c38b747cf40ab
Jaakko Moisio <jaakko@moisio.fi>
2021-11-12 15:17:47 +0200

    Refactor test

--- tests/test_href.py
+++ tests/test_href.py
@@ -1,4 +1,5 @@
 import json
+import typing
 
 from hypothesis import given, strategies as st
 import pydantic
@@ -20,7 +21,7 @@ class Pet(ReferrableModel):
 
 
 class Owner(BaseModel):
-    pet: Href[Pet]
+    pets: typing.List[Href[Pet]]
 
 
 hrefs = st.integers().map(
@@ -30,33 +31,33 @@ hrefs = st.integers().map(
 
 @given(hrefs)
 def test_parse_href(href):
-    assert Owner(pet=href).pet is href
+    assert pydantic.parse_obj_as(Href[Pet], href) is href
 
 
 @given(st.builds(Pet))
 def test_parse_referrable_model(pet):
-    owner = Owner(pet=pet)
-    assert owner.pet.get_key() == pet.id
-    assert owner.pet.get_url() == Pet.key_to_url(pet.id)
+    href = pydantic.parse_obj_as(Href[Pet], pet)
+    assert href.get_key() == pet.id
+    assert href.get_url() == Pet.key_to_url(pet.id)
 
 
 @given(st.integers())
 def test_parse_key_to_href(key):
-    owner = Owner(pet=key)
-    assert owner.pet.get_key() == key
-    assert owner.pet.get_url() == Pet.key_to_url(key)
+    href = pydantic.parse_obj_as(Href[Pet], key)
+    assert href.get_key() == key
+    assert href.get_url() == Pet.key_to_url(key)
 
 
 @given(st.from_regex(r"\A/pets/\d+\Z"))
 def test_parse_url_to_key(url):
-    owner = Owner(pet=url)
-    assert owner.pet.get_key() == Pet.url_to_key(url)
-    assert owner.pet.get_url() == url
+    href = pydantic.parse_obj_as(Href[Pet], url)
+    assert href.get_key() == Pet.url_to_key(url)
+    assert href.get_url() == url
 
 
 def test_parse_href_with_unparseable_key_fails():
     with pytest.raises(pydantic.ValidationError):
-        Owner(pet=object())
+        pydantic.parse_obj_as(Href, object())
 
 
 def test_parse_href_without_parameter_fails():
@@ -64,7 +65,7 @@ def test_parse_href_without_parameter_fails():
         pydantic.parse_obj_as(Href, 123)
 
 
-@given(st.integers())
-def test_json_encode(key):
-    owner_json = json.loads(Owner(pet=key).json())
-    assert owner_json["pet"] == Pet.key_to_url(key)
+@given(st.builds(Owner, pets=st.lists(hrefs)))
+def test_json_encode(owner):
+    owner_json = json.loads(owner.json())
+    assert owner_json["pets"] == [pet.get_url() for pet in owner.pets]
