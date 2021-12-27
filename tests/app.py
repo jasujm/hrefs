e1ba12b825f746b1bdf897e06e47864417e093bf
Jaakko Moisio <jaakko@moisio.fi>
2021-12-27 09:58:59 +0200

    Use self references in the test app

--- tests/app.py
+++ tests/app.py
@@ -1,34 +1,52 @@
-from typing import List, Dict
+from typing import List, Dict, ForwardRef
+import uuid
 
-from fastapi import FastAPI, HTTPException, Request, Response
+from fastapi import FastAPI, HTTPException, Response
 from fastapi.middleware import Middleware
 from pydantic import BaseModel
+from typing_extensions import Annotated
 
-from hrefs import Href
+from hrefs import Href, PrimaryKey
 from hrefs.starlette import ReferrableModel, HrefMiddleware
 
 
+class BookCreate(BaseModel):
+    title: str
+
+
 class Book(ReferrableModel):
-    id: int
+    self: Annotated[Href[ForwardRef("Book")], PrimaryKey(type_=uuid.UUID, name="id")]
     title: str
 
     class Config:
         details_view = "get_book"
 
 
-class Library(BaseModel):
-    id: int
+Book.update_forward_refs()
+
+
+class LibraryCreate(BaseModel):
     books: List[Href[Book]]
 
 
-books: Dict[int, Book] = {}
-libraries: Dict[int, Library] = {}
+class Library(ReferrableModel):
+    self: Annotated[Href[ForwardRef("Library")], PrimaryKey(type_=uuid.UUID, name="id")]
+    books: List[Href[Book]]
+
+    class Config:
+        details_view = "get_library"
+
+
+Library.update_forward_refs()
+
+books: Dict[uuid.UUID, Book] = {}
+libraries: Dict[uuid.UUID, Library] = {}
 
 app = FastAPI(middleware=[Middleware(HrefMiddleware)])
 
 
 @app.get("/libraries/{id}", response_model=Library)
-def get_library(id: int):
+def get_library(id: uuid.UUID):
     library = libraries.get(id)
     if not library:
         raise HTTPException(status_code=404, detail="Library not found")
@@ -36,20 +54,21 @@ def get_library(id: int):
 
 
 @app.post("/libraries")
-def post_library(library: Library, request: Request):
-    if any(book.get_key() not in books for book in library.books):
+def post_library(library: LibraryCreate):
+    if any(book.key not in books for book in library.books):
         raise HTTPException(
             status_code=400, detail="Trying to add nonexisting book to library"
         )
-    libraries[library.id] = library
+    new_library = Library(self=uuid.uuid4(), **library.dict())
+    libraries[new_library.self.key] = new_library
     return Response(
         status_code=201,
-        headers={"Location": request.url_for("get_library", id=library.id)},
+        headers={"Location": new_library.self.url},
     )
 
 
 @app.get("/books/{id}", response_model=Book)
-def get_book(id: int):
+def get_book(id: uuid.UUID):
     book = books.get(id)
     if not book:
         raise HTTPException(status_code=404, detail="Book not found")
@@ -57,8 +76,7 @@ def get_book(id: int):
 
 
 @app.post("/books", status_code=204)
-def post_book(book: Book, request: Request):
-    books[book.id] = book
-    return Response(
-        status_code=201, headers={"Location": request.url_for("get_book", id=book.id)}
-    )
+def post_book(book: BookCreate):
+    new_book = Book(self=uuid.uuid4(), **book.dict())
+    books[new_book.self.key] = new_book
+    return Response(status_code=201, headers={"Location": new_book.self.url})
