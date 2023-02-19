e6b35166c73fb28806e98cff618fceb3d9c4e55d
Jaakko Moisio <jaakko@moisio.fi>
2023-02-19 19:21:58 +0200

    starlette: cache routes for performance

--- hrefs/starlette.py
+++ hrefs/starlette.py
@@ -9,7 +9,7 @@ from starlette.datastructures import URL, QueryParams
 from starlette.requests import HTTPConnection, Request
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.applications import Starlette
-from starlette.routing import BaseRoute
+from starlette.routing import Route
 
 from .model import BaseReferrableModel
 
@@ -19,23 +19,47 @@ _URL_MODEL: typing.Type[pydantic.BaseModel] = pydantic.create_model(
 )
 
 RequestOrApp = typing.Union[HTTPConnection, Starlette]
-BaseUrl = typing.Optional[typing.Union[str, URL]]
+BaseUrl = typing.Union[str, URL]
 
 
 _href_context_var: contextvars.ContextVar[
     typing.Tuple[RequestOrApp, typing.Optional[BaseUrl]]
 ] = contextvars.ContextVar("_href_context_var")
 
+_route_cache: typing.Dict[typing.Tuple[Starlette, str], Route] = {}
 
-def _get_routes_from_context() -> typing.Tuple[typing.Sequence[BaseRoute], BaseUrl]:
+
+def _get_route(route_name: str) -> typing.Optional[Route]:
+    request_or_app, _ = _href_context_var.get()
+    if isinstance(request_or_app, HTTPConnection):
+        app = request_or_app.app
+    else:
+        app = request_or_app
+    cached_route = _route_cache.get((app, route_name))
+    if cached_route:
+        return cached_route
+    for route in app.routes:
+        if isinstance(route, Route) and route.name == route_name:
+            _route_cache[(app, route_name)] = route
+            return route
+    return None
+
+
+def _get_base_url() -> BaseUrl:
     request_or_app, base_url = _href_context_var.get()
     if isinstance(request_or_app, HTTPConnection):
-        return request_or_app.app.routes, request_or_app.base_url
-    return request_or_app.routes, base_url
+        return request_or_app.base_url
+    if not base_url:
+        raise RuntimeError(
+            "href_context must have base_url set if using application as context"
+        )
+    return base_url
 
 
 @contextlib.contextmanager
-def href_context(request_or_app: RequestOrApp, *, base_url: BaseUrl = None):
+def href_context(
+    request_or_app: RequestOrApp, *, base_url: typing.Optional[BaseUrl] = None
+):
     """Context manager that sets hyperlink context
 
     Makes ``request_or_app`` responsible for converting between keys and URLs in
@@ -88,7 +112,7 @@ def href_context(request_or_app: RequestOrApp, *, base_url: BaseUrl = None):
        handlers of a Starlette/FastAPI app, it is recommended to use
        :class:`HrefMiddleware` to automatically set the context.
 
-    Parameter:
+    Arguments:
         request_or_app: The request or app to be used as hyperlink context
         base_url: The base URL (needed when using application as context)
     """
@@ -153,47 +177,40 @@ class ReferrableModel(BaseReferrableModel):
 
     @classmethod
     def key_to_url(cls, key) -> pydantic.AnyHttpUrl:
-        routes, base_url = _get_routes_from_context()
         details_view = cls._get_details_view()
-        if not base_url:
-            raise RuntimeError(
-                "href_context must have base_url set if using application as context"
-            )
-        url: URL
-        for route in routes:
-            if getattr(route, "name", None) == details_view:
-                all_params = cls.key_to_path_params(key)
-                path_param_keys = set(getattr(route, "param_convertors", {}).keys())
-                path_params = {
-                    k: v for (k, v) in all_params.items() if k in path_param_keys
-                }
-                query_params = {
-                    k: v for (k, v) in all_params.items() if k not in path_param_keys
-                }
-                url = URL(
-                    route.url_path_for(details_view, **path_params).make_absolute_url(
-                        base_url
-                    )
-                ).replace_query_params(**query_params)
-                break
-        return _URL_MODEL.parse_obj(str(url)).__root__  # type: ignore
+        route = _get_route(details_view)
+        if route:
+            all_params = cls.key_to_path_params(key)
+            path_param_keys = set(route.param_convertors.keys())
+            path_params = {
+                k: v for (k, v) in all_params.items() if k in path_param_keys
+            }
+            query_params = {
+                k: v for (k, v) in all_params.items() if k not in path_param_keys
+            }
+            url = URL(
+                route.url_path_for(details_view, **path_params).make_absolute_url(
+                    _get_base_url()
+                )
+            ).replace_query_params(**query_params)
+            return _URL_MODEL.parse_obj(str(url)).__root__  # type: ignore
+        raise ValueError("Could not resolve {key} into url")
 
     @classmethod
     def url_to_key(cls, url: pydantic.AnyHttpUrl) -> typing.Any:
-        routes, _ = _get_routes_from_context()
         details_view = cls._get_details_view()
-        for route in routes:
-            if getattr(route, "name", None) == details_view:
-                _, scope = route.matches(
-                    {"type": "http", "method": "GET", "path": url.path}
-                )
-                if scope:
-                    query_params = QueryParams(url.query or "")
-                    all_params = {
-                        **scope["path_params"],
-                        **query_params,
-                    }
-                    return cls.path_params_to_key(all_params)
+        route = _get_route(details_view)
+        if route:
+            _, scope = route.matches(
+                {"type": "http", "method": "GET", "path": url.path}
+            )
+            if scope:
+                query_params = QueryParams(url.query or "")
+                all_params = {
+                    **scope["path_params"],
+                    **query_params,
+                }
+                return cls.path_params_to_key(all_params)
         raise ValueError(f"Could not resolve {url} into key")
 
     @classmethod
